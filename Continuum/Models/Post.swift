7ead93608aa3544c70b437a2c848a2535708a3a4
Jeremy Taylor <jtaylor@jctaylor.org>
2020-12-10 17:08:36 -0600

    Updated Post Model for CloudKit

--- Continuum/Models/Post.swift
+++ Continuum/Models/Post.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import CloudKit
+
+struct PostConstants {
+    static let typeKey = "Post"
+    static let captionKey = "caption"
+    static let timestampKey = "timestamp"
+    static let commentsKey = "comments"
+    static let photoKey = "photo"
+}
 
 class Post {
     var photoData: Data?
     let timestamp: Date
     let caption: String
+    let recordID: CKRecord.ID
     var comments: [Comment]
     
     var photo: UIImage?{
@@ -24,19 +34,45 @@ class Post {
         }
     }
     
-    init(photoData: Data?, timestamp: Date = Date(), caption: String, comments: [Comment] = [], photo: UIImage?) {
+    var imageAsset: CKAsset? {
+        get {
+            let tempDirectory = NSTemporaryDirectory()
+            let tempDirecotryURL = URL(fileURLWithPath: tempDirectory)
+            let fileURL = tempDirecotryURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpg")
+            do {
+                try photoData?.write(to: fileURL)
+            } catch let error {
+                print("Error writing to temp url \(error) \(error.localizedDescription)")
+            }
+            return CKAsset(fileURL: fileURL)
+        }
+    }
+    
+    init(photoData: Data?, timestamp: Date = Date(), caption: String, comments: [Comment] = [], photo: UIImage?, recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
         self.photoData = photoData
         self.timestamp = timestamp
         self.caption = caption
         self.comments = comments
+        self.recordID = recordID
         self.photo = photo
     }
     
     
 } // End of Class
 
+//MARK: - Extensions
+
 extension Post: SearchableRecord {
     func matches(searchTerm: String) -> Bool {
         return caption.lowercased().contains(searchTerm.lowercased())
     }
 } // End of Post extension
+
+extension CKRecord {
+    convenience init?(post: Post) {
+        self.init(recordType: PostConstants.typeKey, recordID: post.recordID)
+        self.setValue(post.caption, forKey: PostConstants.captionKey)
+        self.setValue(post.timestamp, forKey: PostConstants.timestampKey)
+        self.setValue(post.imageAsset, forKey: PostConstants.photoKey)
+    }
+} // End of CKRecord Extension
